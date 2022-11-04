22e22158f0a01f3d478263b52910c544d4d15922
Shubham <kumar.shubham1312@gmail.com>
2022-11-04 21:58:33 +0530

    add a PING response

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 #[allow(unused_imports)]
+use core::panic;
+#[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
+#[allow(unused_imports)]
+use std::net::TcpStream;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
+    let addr = "127.0.0.1:6379";
 
     // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
-        Err(e) => println!("couldn't accept client: {:?}", e),
+    let listener = TcpListener::bind(addr).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                stream.write_fmt(format_args!("+PONG\r\n")).unwrap();
+            }
+            Err(_) => (),
+        }
     }
+
+    Ok(())
+    // match listener.accept() {
+    //     Ok((_socket, addr)) => println!("accepted new client: {:?}", addr),
+    //     Err(e) => println!("couldn't accept client: {:?}", e),
+    // }
 }
